b2a5282969cda80debb216d6cf4cc89ea565d32d
tomas filchtinsky <tomifilchi@hotmail.com>
2024-04-08 20:38:11 -0300

    lista doblemente enlazada

--- modulos_ordenamiento/modulos.py
+++ modulos_ordenamiento/modulos.py
@@ -1,53 +1,50 @@
-#from random import randint
-#def ordenamientoBurbuja(unaLista):
-#    for numPasada in range(len(unaLista)):
-#        for i in range(len(unaLista)-1):
-#            if unaLista[i]>unaLista[i+1]:
-#                unaLista[i],unaLista[i+1] = unaLista[i+1],unaLista[i]
-#unaLista = [randint(1,20) for x in range(10)]
-#print(unaLista)
-##ordenamientoBurbuja(unaLista)
-##print(unaLista)
-#
-#def ordenamientoRapido(unaLista):
-#   ordenamientoRapidoAuxiliar(unaLista,0,len(unaLista)-1)
-#
-#def ordenamientoRapidoAuxiliar(unaLista,primero,ultimo):
-#   if primero<ultimo:
-#
-#       puntoDivision = particion(unaLista,primero,ultimo)
-#
-#       ordenamientoRapidoAuxiliar(unaLista,primero,puntoDivision-1)
-#       ordenamientoRapidoAuxiliar(unaLista,puntoDivision+1,ultimo)
-#
-#
-#def particion(unaLista,primero,ultimo):
-#   valorPivote = unaLista[primero]
-#   marcaIzq = primero+1
-#   marcaDer = ultimo
-#
-#   hecho = False
-#   while not hecho:
-#
-#       while marcaIzq <= marcaDer and unaLista[marcaIzq] <= valorPivote:
-#           marcaIzq = marcaIzq + 1
-#
-#
-#       while unaLista[marcaDer] >= valorPivote and marcaDer >= marcaIzq:
-#           marcaDer = marcaDer -1
-#       if marcaDer < marcaIzq:
-#           hecho = True
-#       else:
-#           unaLista[marcaDer],unaLista[marcaIzq]=unaLista[marcaIzq], unaLista[marcaDer]
-#  
-#   unaLista[primero], unaLista[marcaDer] = unaLista[marcaDer], valorPivote
-#
-#
-#   return marcaDer
-#
-#ordenamientoRapido(unaLista)
-#print(unaLista)
-from random import randint 
+from random import randint
+def ordenamientoBurbuja(unaLista):
+    for numPasada in range(len(unaLista)):
+        for i in range(len(unaLista)-1):
+            if unaLista[i]>unaLista[i+1]:
+                unaLista[i],unaLista[i+1] = unaLista[i+1],unaLista[i]
+unaLista = [randint(1,20) for x in range(10)]
+print(unaLista)
+
+
+def ordenamientoRapido(unaLista):
+   ordenamientoRapidoAuxiliar(unaLista,0,len(unaLista)-1)
+
+def ordenamientoRapidoAuxiliar(unaLista,primero,ultimo):
+   if primero<ultimo:
+
+       puntoDivision = particion(unaLista,primero,ultimo)
+
+       ordenamientoRapidoAuxiliar(unaLista,primero,puntoDivision-1)
+       ordenamientoRapidoAuxiliar(unaLista,puntoDivision+1,ultimo)
+
+
+def particion(unaLista,primero,ultimo):
+   valorPivote = unaLista[primero]
+   marcaIzq = primero+1
+   marcaDer = ultimo
+
+   hecho = False
+   while not hecho:
+
+       while marcaIzq <= marcaDer and unaLista[marcaIzq] <= valorPivote:
+           marcaIzq = marcaIzq + 1
+
+
+       while unaLista[marcaDer] >= valorPivote and marcaDer >= marcaIzq:
+           marcaDer = marcaDer -1
+       if marcaDer < marcaIzq:
+           hecho = True
+       else:
+           unaLista[marcaDer],unaLista[marcaIzq]=unaLista[marcaIzq], unaLista[marcaDer]
+  
+   unaLista[primero], unaLista[marcaDer] = unaLista[marcaDer], valorPivote
+
+
+   return marcaDer
+
+
 
 
 def radixSort(lista):
@@ -79,5 +76,3 @@ listaCadenas = []
 for n in listaNumeros:
     listaCadenas.append(str(n))
 
-print(listaCadenas)
-print(radixSort(listaCadenas))
