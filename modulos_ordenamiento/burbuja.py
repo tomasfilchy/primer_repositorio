7a0261141acf40d53a8499f49b86d85bdf348622
tomasfilchy <88282170+tomasfilchy@users.noreply.github.com>
2024-04-25 21:05:26 -0300

    3 errores

---
+++ modulos_ordenamiento/burbuja.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def ordenamientoBurbuja(unaLista):
+    for numPasada in range(len(unaLista)-1,0,-1):
+        for i in range(numPasada):
+            if unaLista[i]>unaLista[i+1]:
+                temp = unaLista[i]
+                unaLista[i] = unaLista[i+1]
+                unaLista[i+1] = temp
+
+unaLista = [randint(10000, 99999) for x in range(500)]
+ordenamientoBurbuja(unaLista)
+print(unaLista)
